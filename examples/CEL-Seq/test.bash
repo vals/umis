fc8a9505091d0760c1161f5bd16655e7b0e6b370
Valentine Svensson <vale@ebi.ac.uk>
2015-06-15 13:14:59 +0100

    Added a test script and an expected result for CEL-Seq

---
+++ examples/CEL-Seq/test.bash
@@ -0,0 +1,5 @@
+python umicount.py fastqtrim \
+--fastq1 examples/CEL-Seq/SRP036633_1.fastq \
+--fastq2 examples/CEL-Seq/SRP036633_2.fastq \
+--cbs 1 --cbe 8 --mbs 9 --mbe 12 \
+--outfastq examples/CEL-Seq/SRP036633_trimmed.fastq
