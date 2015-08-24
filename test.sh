e481ec8e91faf678c09c1720048ca1a8cc85b414
Valentine Svensson <vale@ebi.ac.uk>
2015-08-24 13:31:33 +0100

    Generalized CEL-Seq transform to allow SRA style reads

--- test.sh
+++ test.sh
@@ -3,52 +3,58 @@ rm test*.fq
 umis fastqtransform \
 examples/MARS-Seq/transform.json \
 examples/MARS-Seq/SRP035326.fastq \
-> test1.fq
+> test01.fq
 
 umis fastqtransform \
 examples/MARS-Seq/transform.json \
 examples/MARS-Seq/SRP035326_5.fastq \
-> test2.fq
+> test02.fq
 
 umis fastqtransform \
 examples/CEL-Seq/transform.json \
 examples/CEL-Seq/SRP036633_1.fastq \
 examples/CEL-Seq/SRP036633_2.fastq \
-> test3.fq
+> test03.fq
 
 umis fastqtransform \
 examples/DropSeq/transform.json \
 examples/DropSeq/SRR1873278_1.fastq \
 examples/DropSeq/SRR1873278_2.fastq \
-> test4.fq
+> test04.fq
 
 umis fastqtransform \
 examples/inDrop/transform.json \
 examples/inDrop/SRR1784317_1.fastq \
 examples/inDrop/SRR1784317_2.fastq \
-> test5.fq
+> test05.fq
 
 umis fastqtransform \
 --demuxed_cb ATTAGAC \
 examples/STRT-Seq/SRP022764_transform.json \
 examples/STRT-Seq/SRP022764_ESCell_1_ATTAGAC_single.fastq \
-> test6.fq
+> test06.fq
 
 umis fastqtransform \
 --demuxed_cb A01 \
 examples/STRT-Seq/SRP045452_transform.json \
 examples/STRT-Seq/SRP045452_1772058148_A01.fastq \
-> test7.fq
+> test07.fq
 
 umis fastqtransform \
 --demuxed_cb CACTGT \
 examples/BATSeq/transform.json \
 examples/BATseq/SRR1558183_1.fastq \
 examples/BATseq/SRR1558183_2.fastq \
-> test8.fq
+> test08.fq
 
 umis fastqtransform \
 examples/CEL-Seq/transform.json \
 examples/CEL-Seq/SRP036633_1.fastq.gz \
 examples/CEL-Seq/SRP036633_2.fastq.gz \
-> test9.fq
+> test09.fq
+
+umis fastqtransform \
+examples/CEL-Seq/transform.json \
+examples/CEL-Seq/SRP048838_1.fastq \
+examples/CEL-Seq/SRP048838_2.fastq \
+> test10.fq
