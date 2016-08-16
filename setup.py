69c34fd8541fcf7c4680f9be3a421aa188cee506
Valentine Svensson <vale@ebi.ac.uk>
2016-08-16 11:20:49 +0100

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
         name='umis',
-        version='0.2.2',
+        version='0.3',
         description='Package for estimating UMI counts in Transcript Tag Counting data.',
         packages=find_packages(),
         install_requires=['click', 'pysam>=0.8.3', 'pandas', 'regex'],
