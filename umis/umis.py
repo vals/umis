b02a11335c38b9a03443c686994f1e40b2548121
roryk <rory.kirchner@gmail.com>
2018-07-11 14:52:43 -0400

    Change version returned by `version` subcommand to 1.0.0

--- umis/umis.py
+++ umis/umis.py
@@ -24,7 +24,7 @@ import numpy as np
 import scipy.io, scipy.sparse
 import click
 
-VERSION = "0.9.0b"
+VERSION = "1.0.0"
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
